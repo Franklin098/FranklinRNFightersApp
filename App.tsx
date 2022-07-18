975e190d7e3f028dd3891e82b84a633bf806213a
Frankin098 <iefranklin@hotmail.es>
2022-07-17 22:20:18 -0600

    add SplashScreen for iOS

--- App.tsx
+++ App.tsx
@@ -1,12 +1,17 @@
 import {View, Text} from 'react-native';
-import React from 'react';
+import React, {useEffect} from 'react';
 import 'react-native-gesture-handler';
 import {NavigationContainer} from '@react-navigation/native';
 import StackNavigator from './src/navigation/StackNavigator';
 import {Provider} from 'react-redux';
 import {store} from './src/reducers/store';
+import SplashScreen from 'react-native-splash-screen';
 
 export default function App() {
+  useEffect(() => {
+    SplashScreen.hide();
+  }, []);
+
   return (
     <Provider store={store}>
       <NavigationContainer>
